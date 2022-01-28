1d6aa1ed8521657663002e6ed913b5afc4cdd082
sena_kuzuoka <senakuzuoka@gmail.com>
2022-01-28 22:58:35 +0900

    直近不要なScript郡を削除

--- Assets/Runtime/Scripts/Timer/TimerExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-public static class TimerExtensions {
-    public static Timer RegisterTimer(this MonoBehaviour owner, float duration, Action onComplete, bool isLooped = false, bool useUnscaledTime = false) {
-        return Timer.Register(duration, onComplete, isLooped, useUnscaledTime, owner);
-    }
-}
